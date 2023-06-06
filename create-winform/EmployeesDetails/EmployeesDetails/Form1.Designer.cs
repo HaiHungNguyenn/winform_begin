274e9e629b86faa6f51d46361366b201be43f0a4
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2023-06-06 14:08:47 +0700

    employee details

---
+++ create-winform/EmployeesDetails/EmployeesDetails/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace EmployeesDetails
+{
+    partial class frmEmployeesDetails: Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        private Label lbTitle;
+        private Label lbEmpID;
+        private Label lbEmpName;
+        private Label lbPhone;
+        private Label lbDegree;
+        private TextBox txtEmployeeID;
+        private TextBox txtEmployeeName;
+        private MaskedTextBox mtxtPhone;
+        private ComboBox cboDegree;
+        private Button btnSave;
+        private Button btnCancel;
+
+        public frmEmployeesDetails() => InitializeComponent();
+
+
+
+
+        private void InitializeComponent()
+        {
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lbEmpID = new System.Windows.Forms.Label();
+            this.lbEmpName = new System.Windows.Forms.Label();
+            this.lbPhone = new System.Windows.Forms.Label();
+            this.lbDegree = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.mtxtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.cboDegree = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lbGender = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(10, 10);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(200, 30);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Title";
+            // 
+            // lbEmpID
+            // 
+            this.lbEmpID.Location = new System.Drawing.Point(10, 50);
+            this.lbEmpID.Name = "lbEmpID";
+            this.lbEmpID.Size = new System.Drawing.Size(100, 20);
+            this.lbEmpID.TabIndex = 1;
+            this.lbEmpID.Text = "Employee ID";
+            // 
+            // lbEmpName
+            // 
+            this.lbEmpName.Location = new System.Drawing.Point(10, 80);
+            this.lbEmpName.Name = "lbEmpName";
+            this.lbEmpName.Size = new System.Drawing.Size(100, 20);
+            this.lbEmpName.TabIndex = 2;
+            this.lbEmpName.Text = "Employee Name";
+            // 
+            // lbPhone
+            // 
+            this.lbPhone.Location = new System.Drawing.Point(10, 110);
+            this.lbPhone.Name = "lbPhone";
+            this.lbPhone.Size = new System.Drawing.Size(100, 20);
+            this.lbPhone.TabIndex = 3;
+            this.lbPhone.Text = "Phone";
+            // 
+            // lbDegree
+            // 
+            this.lbDegree.Location = new System.Drawing.Point(10, 170);
+            this.lbDegree.Name = "lbDegree";
+            this.lbDegree.Size = new System.Drawing.Size(100, 20);
+            this.lbDegree.TabIndex = 5;
+            this.lbDegree.Text = "Degree";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(120, 50);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(200, 20);
+            this.txtEmployeeID.TabIndex = 6;
+            this.txtEmployeeID.TextChanged += new System.EventHandler(this.txtEmployeeID_TextChanged);
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(120, 80);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(200, 20);
+            this.txtEmployeeName.TabIndex = 7;
+            // 
+            // mtxtPhone
+            // 
+            this.mtxtPhone.Location = new System.Drawing.Point(120, 110);
+            this.mtxtPhone.Mask = "000-0000000";
+            this.mtxtPhone.Name = "mtxtPhone";
+            this.mtxtPhone.Size = new System.Drawing.Size(100, 20);
+            this.mtxtPhone.TabIndex = 8;
+            // 
+            // cboDegree
+            // 
+            this.cboDegree.Items.AddRange(new object[] {
+            "---Select Degree---",
+            "Ph. D.",
+            "Master",
+            "Engineer",
+            "Bachelor",
+            "Technician"});
+            this.cboDegree.Location = new System.Drawing.Point(120, 170);
+            this.cboDegree.Name = "cboDegree";
+            this.cboDegree.Size = new System.Drawing.Size(200, 21);
+            this.cboDegree.TabIndex = 12;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(10, 300);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "&Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(100, 300);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lbGender
+            // 
+            this.lbGender.Location = new System.Drawing.Point(7, 220);
+            this.lbGender.Name = "lbGender";
+            this.lbGender.Size = new System.Drawing.Size(100, 20);
+            this.lbGender.TabIndex = 13;
+            this.lbGender.Text = "Gender";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdFemale);
+            this.groupBox1.Controls.Add(this.rdMale);
+            this.groupBox1.Location = new System.Drawing.Point(120, 220);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 45);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Checked = true;
+            this.rdMale.Location = new System.Drawing.Point(19, 24);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(48, 17);
+            this.rdMale.TabIndex = 0;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(108, 24);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdFemale.TabIndex = 1;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+            // frmEmployeesDetails
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(350, 350);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbGender);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.lbEmpID);
+            this.Controls.Add(this.lbEmpName);
+            this.Controls.Add(this.lbPhone);
+            this.Controls.Add(this.lbDegree);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.mtxtPhone);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.cboDegree);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmEmployeesDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Details";
+            this.Load += new System.EventHandler(this.frmEmployeesDetails_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private Label lbGender;
+        private GroupBox groupBox1;
+        private RadioButton rdFemale;
+        private RadioButton rdMale;
+    }
+}
+
---
+++ create-winform/EmployeesDetails/EmployeesDetails/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeesDetails
+{
+    public partial class frmEmployeesDetails : Form
+    {
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmployeeID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmEmployeesDetails_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            String EmployeeID = txtEmployeeID.Text;
+            String EmployeeNme = txtEmployeeName.Text;
+            String Phone = mtxtPhone.Text;
+            String Gender = (rdFemale.Checked ? "Female" : "Male");
+            String Degree = cboDegree.Text;
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"Employee ID:{EmployeeID}\n");
+            builder.Append($"Employee Name:{EmployeeNme}\n");
+            builder.Append($"Phone:{Phone}\n");
+            builder.Append($"Gender:{Gender}\n");
+            builder.Append($"Degree ID:{Degree}\n");
+            MessageBox.Show(builder.ToString(),"Employee Details");
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
